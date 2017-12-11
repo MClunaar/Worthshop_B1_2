91ddcc6f5a0a8ac9fe13e6f928872217d950570b
AeroCloud27 <34441780+AeroCloud27@users.noreply.github.com>
2017-12-11 17:12:40 +0100

    Add files via upload

---
+++ orange/deconnexion.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+	<meta http-equiv="refresh" content="0; URL=site_orange.php">
+	<?php 
+		session_start();
+		session_destroy();
+	?>
+</head>
+
+</html>
---
+++ orange/site_orange_admin.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<link href="site_orange.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<div class="banner">
+			<div class="bannercontent">
+				<div class="titre">
+					<a class="titlebutton" href="site_orange.php">Médiathèque d'orange </a>
+				</div>				
+			</div>
+		
+			
+		</div>
+		<center>
+		<div class="form_connexion">		
+			<form method = "post" action = "site_orange.php">
+					<input type="text" name="identifiant" placeholder="Identifiant" size="10" class="champs"/>
+					</br>
+
+					<input type="password" name="password" placeholder="Mot de passe" size="10" class="champs"/>
+					</br>
+
+					<input type="submit" name="valider" size="20"  value="Connexion" class="champs"/>	
+
+			</form>
+		</div>
+		</center>
+		
+				
+	
+	</body>
+	
+</html>
