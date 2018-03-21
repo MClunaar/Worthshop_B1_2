3d8d8862da217433f480005c20c558d444ca7a32
AeroCloud27 <34441780+AeroCloud27@users.noreply.github.com>
2018-03-21 12:31:37 +0100

    Add files via upload

---
+++ Workshop/index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<HTML>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div class="container-fluid">    
+        <div class="row strip">
+            <div class="col-3">
+                <div class="row strip">
+                    <div class="col-10 connexion">
+                        <a href="index.php">
+                            <div class=connect>
+                                Se connecter
+                            </div>
+                        </a>
+                        <a href="index.php">
+                            <div class="register">
+                                S'inscrire
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-2">
+                    </div>
+                </div>
+            </div> 
+
+            <div class="col-6 ">
+                <div class="searchbar">
+
+                </div> 
+            </div> 
+            
+            <div class="col-3">
+                <div class="row strip">
+
+                    <div class="col-5">
+                    </div>
+
+                    <div class="col-7 udepsi_strip">
+                        <a href = "index.php" class="mainpagebutton">
+                            <img class="epsi_logo" src="epsi_l.png" alt="logo epsi"/>
+                            <img class="udepsi_logo" src="logo.png" alt"logo epsi"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>   
+
+        <div class="row main" style="margin-left=1vw !important">
+            <div id="test"class="col-10" style="text-align: center">
+                <div class="content-main">
+                    <div class="course">
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+                    <div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+                    <div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+                    <div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div><div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+                    <div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+                    <div class="course">
+
+                        <img class="course-tech" src="php.png"/> 
+                        <div class="course-title">
+                            Titre du cours
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-desc">
+                            Lorem ipsum dolot sir amet deus ameno tot si veritum rectum nostrum i bien ein berliner
+                        </div>
+
+                        <div class="course-line">
+                        </div>
+
+                        <div class="course-bottom">
+                            Par : Un fils de pute
+                            <img class="course-link" src="php.png"/> 
+                        </div>
+                    </div>
+
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                    <div class="course">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-2" style="padding:0px">
+                <div class="row" style="height:100%">
+                    <div class="col-3">
+                        
+                    </div>
+                    <div class="col-9" style="padding:0px">
+                        <div class="right-panel">
+                            <div class="right-panel-top">
+                                
+                            </div>
+                            <div class="right-panel-bottom">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer" >
+            Ceci est le pied-de-page
+        </div>
+
+    </div>
+
+    
+
+    
+
+    <!--
+        360/640
+        480/848
+        720/1280
+        1080/1920
+    -->
+
+</body>
+
+</HTML>
